55cecac11e032e5eccfb0f2e7be55c418f803ba3
Tommy Susanto <tommy88susanto@gmail.com>
2017-03-23 21:13:44 +1100

    secret

--- config/prod.exs
+++ config/prod.exs
@@ -66,4 +66,4 @@ config :heroku_painter_starter, HerokuPainterStarter.Repo,
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
