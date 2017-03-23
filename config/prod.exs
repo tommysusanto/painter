711f07dd920b53da5cfac17d1b5bda4c1606653f
Tommy Susanto <tommy88susanto@gmail.com>
2017-03-23 21:12:08 +1100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :heroku_painter_starter, HerokuPainterStarter.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "fierce-depths-77200.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :heroku_painter_starter, HerokuPainterStarter.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
