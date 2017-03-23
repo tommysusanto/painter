f2a66120d1b00d240dcfebceda04aef2dd4bd1fb
Tommy Susanto <tommy88susanto@gmail.com>
2017-03-23 21:15:41 +1100

    bla

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule HerokuPainterStarter.UserSocket do
   # channel "room:*", HerokuPainterStarter.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
     timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
